78a4f7fd4770306fc5240816aff91abe2f16e8f1
Manoj Karvannan <mkarvann@uab.edu>
2024-10-22 11:19:03 -0500

    main_vecstack[MK]

--- main_test.cpp
+++ main_test.cpp
@@ -5,13 +5,19 @@ using std::cout;
 using std::endl;
 
 
-int main(){
+int main()
+{
     CharStack stack;
     stack.push('a');
     stack.push('b');
-    cout << stack.pop() << " | should be b" << endl;
-    cout << "size (1): " << stack.size() << endl;
+    stack.push('c');
+    cout << "size: " << stack.size() << endl;
     cout << "peeking: " << stack.peek() << endl;
+    cout << stack.pop() << " | should be c" << endl;
+    cout << "size: " << stack.size() << endl;
+    cout << "peeking: " << stack.peek() << endl;
+    stack.clear();
+    cout << "stack size after clearing: " << stack.size() << std::endl;
 
 
     return 0;
