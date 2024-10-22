6069e574e5f4e1c849ab7e271ac7764844fdc9a9
John Bedingfield <john.bedingfield@yahoo.com>
2024-10-22 10:33:25 -0500

    add peek, pop

--- charstack.cpp
+++ charstack.cpp
@@ -3,8 +3,19 @@
 #include "charstack.h"
 
 
-int main() {
 
 
-    return 0;
+void CharStack::push(char ch){
+    this->stack->push_back(ch);
+    return;
+}
+
+char CharStack::pop(){
+    char thisChar = this->stack->at(this->size() - 1);
+    this->stack->pop_back();
+    return thisChar;
+}
+
+char CharStack::peek(){
+    return this->stack->at(this->size() - 1);
 }
