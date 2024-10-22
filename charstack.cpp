20be386b566ca05e5f433f152a904dbe5920449e
Manoj Karvannan <mkarvann@uab.edu>
2024-10-22 10:56:32 -0500

    Implementation_vecstack[MK]

--- charstack.cpp
+++ charstack.cpp
@@ -3,19 +3,50 @@
 #include "charstack.h"
 
 
-// do the other
 
-void CharStack::push(char ch){
+CharStack::CharStack() 
+{
+    stack = new std::vector<char>(); 
+}
+
+
+CharStack::~CharStack() 
+{
+    delete stack; 
+}
+
+
+int CharStack::size()
+{
+    return stack->size();
+}
+
+
+bool CharStack::isEmpty() 
+{
+    return stack->empty();
+}
+
+
+void CharStack::clear() 
+{
+    stack->clear();
+}
+
+void CharStack::push(char ch)
+{
     this->stack->push_back(ch);
     return;
 }
 
-char CharStack::pop(){
+char CharStack::pop()
+{
     char thisChar = this->stack->at(this->size() - 1);
     this->stack->pop_back();
     return thisChar;
 }
 
-char CharStack::peek(){
+char CharStack::peek()
+{
     return this->stack->at(this->size() - 1);
 }
